bbdc085199b9fc914d0b339a707caa865ce842ca
Susana Santos Moreno <susanasantosmoreno@MacBook-Pro-de-Susana.home>
2020-11-19 20:19:34 +0100

    Init Commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+function App() {
+  return (
+    <div className='container mt-3'>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
